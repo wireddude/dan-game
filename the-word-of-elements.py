d7a3157f3694aabef3cbec476be6557b8b10f777
David Kanter <david@davidkanter.com>
2014-12-28 14:57:08 -0800

    started looping with index to choose players

--- the-word-of-elements.py
+++ the-word-of-elements.py
@@ -1,18 +1,18 @@
-# import sys
-# import codecs
-# from colorsys import hsv_to_rgb
-
 # Define the Players
 
 Players=['Tom','Korto', 'Jack', 'Nathaniel','David','Jerry The Warrier', 'Noah', 'Jordan']
+NumPlayers = len (Players)
 Weapons=['Simple Sword', 'First Shotgun', 'Stop Shield', 'Hand and half sword', 'Laser Gun', 'Forcefield']
+NumWeapons = len (Weapons)
 World=((100,100))
 
 print("Welcome to the World of Elements! Enter if you Dare! This Game was Created by BlazeSlayer1234 and Wireddude73 \n")
 
-print("Here are a list of possible Players")
-for x in Players:
-	print (x)
+print("Here are a list of possible Players to choose from:")
+for x in range (0, NumPlayers):
+	print ('%s-%s' % (x,Players[x]))
+
+
 
 print ("\n")
 
