ca60be5f07c58cf7c9f0379b87ff016d52de53be
David Kanter <david@davidkanter.com>
2014-12-28 14:27:08 -0800

    Added for loop to show list of characters

--- the-word-of-elements.py
+++ the-word-of-elements.py
@@ -1,11 +1,16 @@
-import sys
-import codecs
-from colorsys import hsv_to_rgb
+# import sys
+# import codecs
+# from colorsys import hsv_to_rgb
 
 # Define the Players
 
 Players=['Korto', 'Customize']
 Weapons=['Simple Sword', 'First Shotgun', 'Stop Shield', 'Hand and half sword', 'Laser Gun', 'Forcefield']
+World=((100,100))
 
 print("Welcome to the World of Elements!")
+
+for x in Players:
+	print (x)
+
 
