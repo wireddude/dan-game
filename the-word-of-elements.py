1e4bf19980d58bf14dc9c7c54a53fd00b4a5e461
David Kanter <david@davidkanter.com>
2014-12-28 14:40:28 -0800

    Added more players, like Tom

--- the-word-of-elements.py
+++ the-word-of-elements.py
@@ -4,13 +4,20 @@
 
 # Define the Players
 
-Players=['Korto', 'Customize']
+Players=['Tom','Korto', 'Jack', 'Nathaniel','David','Jerry The Warrier', 'Noah', 'Jordan']
 Weapons=['Simple Sword', 'First Shotgun', 'Stop Shield', 'Hand and half sword', 'Laser Gun', 'Forcefield']
 World=((100,100))
 
-print("Welcome to the World of Elements!")
+print("Welcome to the World of Elements! Enter if you Dare! This Game was Created by BlazeSlayer1234 and Wireddude73 \n")
 
+print("Here are a list of possible Players")
 for x in Players:
 	print (x)
+
+print ("\n")
+
+print("Here is a list of possible weapons")
+for i in Weapons:
+	print (i)
 
 
