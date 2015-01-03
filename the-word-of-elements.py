3dbd97ede94ede383bc55975022666929b373b50
David Kanter <david@davidkanter.com>
2015-01-03 09:29:58 -0800

    fixed len bug and remove app pygame libs

--- the-word-of-elements.py
+++ the-word-of-elements.py
@@ -5,10 +5,9 @@
 import sys
 import random
 import os
-import pygame
 import time
 import pickle
-from pygame.locals import *
+
 
 # check if game data file exists before asking user all their info, right now we don't do anything with the value.
 SaveFilePresent =  os.path.isfile("save.dat")
@@ -16,7 +15,7 @@ SaveFilePresent =  os.path.isfile("save.dat")
 # Get the Time, Define the Players
 Time = time.asctime()
 Players=['Tom','Korto', 'Jack', 'Nathaniel','David the Wimpy Kid','Jerry The Warrier', 'Noah', 'Jordan', 'David the Great']
-NumPlayers = len (Players)
+NumPlayers = len (Players) -1
 
 # Define Weapons
 
@@ -49,70 +48,3 @@ pickle.dump(game_data, save_file)
 save_file.close()
 
 
-## initialize graphics stuff
-
-pygame.init()
-DISPLAYSURF=pygame.display.set_mode((400,400))
-FPS=30
-fpsClock = pygame.time.Clock()
-catImg = pygame.image.load('cat.png')
-catx=40
-caty=40
-direction='right'
-RED = (200,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-
-WarningMessage = "Beware of the Kitty"
-# PersonalWarmingMessage = PlayerName ", " WarningMessage
-
-fontObj = pygame.font.Font('freesansbold.ttf',32)
-textSurfaceObj= fontObj.render(WarningMessage , True, GREEN, BLUE)
-textRectObj = textSurfaceObj.get_rect()
-textRectObj.center = (200,150)
-
-
-# tried to draw a pixel here but causes a seg fault 11 on OSX. 
-# pixObj = pygame.PixelArray(DISPLAYSURF) 
-
-
-
-
-
-
-pygame.display.set_caption('The World Of Elements')
-
-## the main while loop for the game
-
-while True:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
-	pygame.display.update()
-	pygame.draw.rect(DISPLAYSURF, GREEN, (20,20,350,350))
-	
-	if direction == 'right':
-		catx +=5
-		if catx == 250:
-			direction='down'
-	elif direction == 'down':
-		caty +=5
-		if caty == 250:
-			direction='left'
-	elif direction == 'left':
-		catx-=5
-		if catx == 40:
-			direction = 'up'
-	elif direction  == 'up':
-		caty-=5
-		if caty == 40:
-			direction='right'
-
-	DISPLAYSURF.blit(catImg, (catx, caty))
-	DISPLAYSURF.blit(textSurfaceObj, textRectObj)
-
-
-#	pixObj[480][380] = BLACK
-	
