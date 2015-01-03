ea077a4c233d2f0757994e1847871923d02ad9ba
David Kanter <david@davidkanter.com>
2015-01-03 13:26:36 -0800

    made Players a list of tuples

--- the-word-of-elements.py
+++ the-word-of-elements.py
@@ -12,9 +12,20 @@ import pickle
 # check if game data file exists before asking user all their info, right now we don't do anything with the value.
 SaveFilePresent =  os.path.isfile("save.dat")
 
+Players = [
+ {'Name':'Korto','Strength': 75, 'Speed': 56, 'Magic': 27, 'Dexterity': 89 },
+ {'Name':'Tom','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 } ,
+ {'Name':'Jack','Strength': 15, 'Speed': 96, 'Magic': 57, 'Dexterity': 57 },
+ {'Name':'Nathaniel','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 },
+ {'Name':'David','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 },
+ {'Name':'Jerry','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 },
+ {'Name':'Noah','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 },
+ {'Name':'Jordan','Strength': 35, 'Speed': 86, 'Magic': 77, 'Dexterity': 29 } ]
+
+
 # Get the Time, Define the Players
 Time = time.asctime()
-Players=['Tom','Korto', 'Jack', 'Nathaniel','David the Wimpy Kid','Jerry The Warrier', 'Noah', 'Jordan', 'David the Great']
+# Players=[Tom, Korto, Jack, Nathaniel, David, Jerry, Noah, Jordan]
 NumPlayers = len (Players) -1
 Monsters=['Creeper','Enderman', 'Killer Squid', 'Fire Dragon','Killer Kitty','Jerry The Monster', 'Larry the Killer Bug', 'Baby Monster Dragon', 'Zombie']
 
@@ -36,7 +47,7 @@ os.system('clear')
 print("Welcome to the World of Elements! Enter if you Dare! This Game was Created by BlazeSlayer1234 and Wireddude73 \n")
 print("Here are a list of possible players to choose from:")
 for x in range (0, NumPlayers):
-	print ('%s-%s' % (x,Players[x]))
+	print ('%s-%s' % (x,Players[x]['Name']))
 print ("\nPlease choose a number for your player:")
 PlayerNameChoice =  int(sys.stdin.readline())
 PlayerName = Players[PlayerNameChoice]
